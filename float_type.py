5ca2283c784fa44e15cf08c2300a2923bb479e9e
edilks228 <edu007super@gmail.com>
2023-06-30 01:12:52 +0600

    float data type part 2

--- float_type.py
+++ float_type.py
@@ -40,3 +40,56 @@ def solve_quadratic_equation(a, b, c):
         root1 = complex(real_part, imaginary_part)
         root2 = complex(real_part, -imaginary_part)
         return root1, root2
+
+
+def find_value_first(n):
+    y = 1 - (1/2)
+    for i in range(3,n+1):
+        y *= (1 -(1/i))
+
+    return y
+
+# print(find_value_first(6))
+
+
+def find_value_second(n,x):
+    y = math.sin(x)
+    for i in range(2,n+1):
+        y+=math.sin(x**i)
+
+    return y
+
+# print(find_value_second(6,4))
+
+def find_value_third(x):
+    return abs(x) + x**4
+
+
+# print(find_value_third(3))
+def find_values_fourth(x):
+    return abs(x)+ 4 * (x**3) -7*(x**2)
+
+print(find_values_fourth(1))
+def approximate_values_of_infinite_sums():
+    y = 0
+    for i in range(1,11):
+        y += 1/(i * (i+1))
+    return f'approximate values of infinite sums: {y}'
+
+print(approximate_values_of_infinite_sums())
+
+
+def approximate_values_of_infinite_sums_second():
+    y = 0
+    for i in range(1,11):
+        y += 1/(i * (i+2))
+    return f'approximate values of infinite sums: {y}'
+
+print(approximate_values_of_infinite_sums_second())
+
+def approximate_values_of_infinite_sums_third():
+    y = 0
+    for i in range(1,11):
+        y += 1/(i * (i+1) * (i+2))
+        return f'approximate values of infinite sums: {y}'
+print(approximate_values_of_infinite_sums_third())
